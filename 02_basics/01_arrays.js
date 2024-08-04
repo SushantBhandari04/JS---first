5334cef4a39525dd1eb626a5fb044f0882ebc9b5
SushantBhandari04 <sushbh2004@gmail.com>
2024-08-04 18:39:52 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,16 @@
+// array
+
+const myArr = [0,1,2,3,4,5]
+const myHeros = ["Shaktiman", "Naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[3])
+
+// Array methods
+
+// myArr.push(600)
+// myArr.push(100)
+// myArr.pop()
+// console.log(myArr)
+
+ 
