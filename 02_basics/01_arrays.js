cde8127b42b456091d52978f1e2e3985db065e87
SushantBhandari04 <sushbh2004@gmail.com>
2024-08-05 17:35:21 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -11,6 +11,24 @@ const myArr2 = new Array(1,2,3,4)
 // myArr.push(600)
 // myArr.push(100)
 // myArr.pop()
+
+myArr.unshift(100)  // adds element at the start
+myArr.shift()  // removes element from the start 
+
+// console.log(myArr.includes(100))  // check if element is present in the array
+// console.log(myArr.indexOf(2))   // return index of element
+
+const newArr = myArr.join()  // convert array into string
+// console.log(newArr)
+// console.log(typeof newArr)
 // console.log(myArr)
 
+ //  slice and splice
+ const arr2 = myArr.slice(1,3)  // does not manipulate the original array
+ const arr3 = myArr.splice(1,3)  // manipulates original array  =>  removes elements from the original array
+ console.log(arr2);
+ console.log(arr3);
+ console.log(myArr);
+ 
+ 
  
---
+++ 02_basics/02_array.js
@@ -0,0 +1,27 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+// marvel_heros.push(dc_heros)
+// console.log(marvel_heros)
+
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
+
+// const all_Heros = [...marvel_heros, ...dc_heros]  // spread operator
+// console.log(all_Heros);
+
+const another_array = [1,2,3,[4,5],[6,[7,8]]]
+// console.log(another_array.flat(Infinity));
+
+// console.log(Array.isArray("Sushant"));
+// console.log(Array.from("sushant"));  // convert element to array
+
+// console.log(Array.from({name: "sushant"})); // interesting case
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));  // return new array from set of elements
+
+
+
