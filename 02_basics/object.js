e212eb3de2789b2f9be1408408575cbcc3fc9038
SushantBhandari04 <sushbh2004@gmail.com>
2024-08-05 17:52:58 +0000

    object

---
+++ 02_basics/object.js
@@ -0,0 +1,13 @@
+// singleton
+// Object.create
+
+// object literals
+const JsUser = {
+    name: "Sushant",
+    age: 20,
+    location: "Uttarkashi",
+    email: "sushant@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
