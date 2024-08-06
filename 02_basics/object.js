1bb740fe49b17ea8a86e6492e61103b0a0575039
SushantBhandari04 <sushbh2004@gmail.com>
2024-08-06 07:53:11 +0000

    object

--- 02_basics/object.js
+++ 02_basics/object.js
@@ -2,12 +2,38 @@
 // Object.create
 
 // object literals
+
+const mySym = Symbol("key1")
+
 const JsUser = {
     name: "Sushant",
+    "full name": "Sushant Bhandari",
+    // mySym: "mykey1",  // it is not getting used as symbol
+    [mySym]: "mykey1", 
     age: 20,
     location: "Uttarkashi",
     email: "sushant@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+// console.log(JsUser["full name"]);
+// console.log(JsUser[mySym]);
+
+JsUser.email = "sushant@chatgpt.com"
+// Object.freeze(JsUser)  // freeze the data of the object
+JsUser.email = "sushant@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log("Hello JS user");
+}
+JsUser.greeting2 = function(){
+    console.log(`Hello JS user, ${this.name}`);
+    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greeting2());
 
