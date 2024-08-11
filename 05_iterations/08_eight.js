4749e15fe5483df56e002a93bf7880be6c790749
SushantBhandari <sushbh2004@gmail.com>
2024-08-11 12:14:18 +0530

    eight

--- 05_iterations/08_eight.js
+++ 05_iterations/08_eight.js
@@ -8,3 +8,25 @@ const nums = [1,2,3,4,5]
 const total = nums.reduce( (acc,curr) => acc+curr,0)
 
 console.log(total)
+
+const shoppingCart = [
+    {
+        courseName: "python",
+        price: 999
+    },
+    {
+        courseName: "java",
+        price: 2999
+    },
+    {
+        courseName: "javascript",
+        price: 3999
+    },
+    {
+        courseName: "AI",
+        price: 6999
+    }
+]
+
+const priceToPay = shoppingCart.reduce( (acc,item) => acc+item.price,0)
+console.log(priceToPay)
